35530115f58274cbc42e6c4196e4261152f917bd
JulyYu <yuhaocan@weidian.com>
2023-05-26 15:22:28 +0800

    do: update demo

--- app/src/main/java/com/julyyu/learn/opengl/demo/GLDemoActivity.kt
+++ app/src/main/java/com/julyyu/learn/opengl/demo/GLDemoActivity.kt
@@ -1,13 +1,20 @@
 package com.julyyu.learn.opengl.demo
 
+import android.graphics.Bitmap
+import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.julyyu.learn.opengl.demo.texturemap.TextureMapGLView
 import com.julyyu.learn.opengl.demo.texturemap.TextureMapGLView2
 import com.julyyu.learn.opengl.demo.texturemap.TextureMapGLView3
 import com.julyyu.learn.opengl.demo.texturemap.TextureMapGLView4
 import com.julyyu.learn.opengl.demo.texurecut.TextureCutGLView
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
 
 /**
  * @author julyyu
@@ -47,7 +54,43 @@ class GLDemoActivity : AppCompatActivity() {
                     glView = TextureCutGLView(this@GLDemoActivity)
                 }
             }
-            setContentView(glView)
+
+            setContentView(RelativeLayout(this@GLDemoActivity).apply {
+                addView(glView)
+                addView(Button(this@GLDemoActivity).apply {
+                    text = "获取画面bitmap"
+                    setOnClickListener {
+                        glView?.apply {
+                            val width: Int = getWidth()
+                            val height: Int = getHeight()
+                            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+                            queueEvent(Runnable { // 创建一个ByteBuffer对象来存储像素数据
+                                val buffer: ByteBuffer = ByteBuffer.allocateDirect(width * height * 4)
+                                buffer.order(ByteOrder.nativeOrder())
+
+                                // 从OpenGL渲染缓冲区中读取像素数据
+                                GLES20.glReadPixels(
+                                    0,
+                                    0,
+                                    width,
+                                    height,
+                                    GLES20.GL_RGBA,
+                                    GLES20.GL_UNSIGNED_BYTE,
+                                    buffer
+                                )
+
+                                // 将像素数据复制到Bitmap对象中
+                                buffer.rewind()
+                                bitmap.copyPixelsFromBuffer(buffer)
+                                print("!!!!")
+                            })
+                        }
+
+                    }
+                }, RelativeLayout.LayoutParams(-2, -2).apply {
+                    addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
+                })
+            })
         }
     }
 }
